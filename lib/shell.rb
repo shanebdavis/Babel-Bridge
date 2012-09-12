8a6fb3043aa9dd35d213e436420a069b46e182f3
Shane Brinkman-Davis <shanebdavis@gmail.com>
2012-09-12 14:33:13 -0700

    exception-guareded parse-tree-node evaluation

--- lib/shell.rb
+++ lib/shell.rb
@@ -7,22 +7,28 @@ class Shell
     @parser = parser
   end
 
+  def evaluate(parse_tree_node)
+    parse_tree_node.evaluate
+  rescue Exception => e
+    @stderr.puts "Error evaluating parse tree: #{e}\n  "+e.backtrace.join("\n  ")
+  end
+
   # if block is provided, successful parsers are yield to block
   # Otherwise, succuessful parsers are sent the "eval" method
   def start(options={},&block)
-    stdout = options[:stdout] || $stdout
-    stderr = options[:stdout] || stdout
-    stdin = options[:stdin] || $stdin
-    while line = Readline.readline("> ", true)
+    @stdout = options[:stdout] || $stdout
+    @stderr = options[:stdout] || @stdout
+    @stdin = options[:stdin] || $stdin
+    while line = @stdin == $stdin ? Readline.readline("> ", true) : @stdin.gets
       ret = parser.parse line.strip
       if ret
         if block
           yield ret
         else
-          stdout.puts " => #{ret.evaluate.inspect}"
+          @stdout.puts " => #{evaluate(ret).inspect}"
         end
       else
-        stderr.puts parser.parser_failure_info
+        @stderr.puts parser.parser_failure_info
       end
     end
   end
