85934ee96628d67f9478425d3690f31bcfce99ce
Shane Brinkman-Davis <shanebdavis@gmail.com>
2012-10-14 11:47:56 -0700

    shell is now more friendly to custom evaluation

--- lib/shell.rb
+++ lib/shell.rb
@@ -7,19 +7,42 @@ class Shell
     @parser = parser
   end
 
-  def evaluate(parse_tree_node)
-    if parse_tree_node.respond_to? :evaluate
-      " => "+parse_tree_node.evaluate.inspect
-    else
-      "\nParse tree:\n  "+
-      parse_tree_node.inspect.gsub("\n","\n  ")+"\n\n"
-    end
-  rescue Exception => e
-    @stderr.puts "Error evaluating parse tree: #{e}\n  "+e.backtrace.join("\n  ")+"\nParse Tree:\n"+parse_tree_node.inspect
+  def pretty_print_parse_tree(parse_tree_node)
+    puts "\nParse tree:\n  #{parse_tree_node.inspect.gsub("\n","\n  ")}\n\n"
   end
 
-  # if block is provided, successful parsers are yield to block
-  # Otherwise, succuessful parsers are sent the "eval" method
+  # Providing a block overrides all evaluate behavor and simply:  yield parse_tree_node, self
+  # elsif parse_tree_node responds to "evaluate":                 puts_result parse_tree_node.evaluate
+  # else                                                          pretty-print the parse-tree
+  # rescue and pretty-print errors
+  def evaluate(parse_tree_node, &block)
+    if block
+      yield parse_tree_node, self
+    elsif parse_tree_node.respond_to? :evaluate
+      puts_result parse_tree_node.evaluate
+    else
+      pretty_print_parse_tree parse_tree_node
+    end
+  rescue Exception => e
+    errputs "Error evaluating parse tree: #{e}\n" + ["Backtrace:",e.backtrace].flatten.join("\n  ")
+    pretty_print_parse_tree parse_tree_node
+  end
+
+  def puts(str)
+    @stdout.puts str
+  end
+
+  def puts_result(str)
+    @stdout.puts " => "+str.inspect
+  end
+
+  def errputs(str)
+    @stderr.puts str
+  end
+
+  # Each line of input is parsed.
+  # If parser fails, output explaination of why.
+  # If parser succeeds, evaluate parse_tree_node, &block
   def start(options={},&block)
     @stdout = options[:stdout] || $stdout
     @stderr = options[:stdout] || @stdout
@@ -27,15 +50,11 @@ class Shell
     while line = @stdin == $stdin ? Readline.readline("> ", true) : @stdin.gets      
       line.strip!
       next if line.length==0
-      ret = parser.parse line
-      if ret
-        if block
-          yield ret
-        else
-          @stdout.puts evaluate(ret)
-        end
+      parse_tree_node = parser.parse line
+      if parse_tree_node
+        evaluate parse_tree_node, &block
       else
-        @stderr.puts parser.parser_failure_info :verbose => true
+        errputs parser.parser_failure_info :verbose => true
       end
     end
   end
